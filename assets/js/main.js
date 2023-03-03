4bf3873608e496b1a985c84ceff9fd2e67e8132d
Ubaidullah <researchfigma@gmail.com>
2023-03-03 18:24:13 +0500

    reponsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,13 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay: '400',
+})
+
+sr.reveal(`.profile--border`)
+sr.reveal(`.profile--name`,{delay: 500})
+sr.reveal(`.profile--name`,{delay: 500})
